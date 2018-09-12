0d6d897126f7bd23e2b517912e3a73e1e0b6bf13
imfl <lei.fu@connect.ust.hk>
2018-09-12 13:57:46 +0800

    dye-how

--- examples/how.cpp
+++ examples/how.cpp
@@ -19,26 +19,25 @@ ostream & operator<<(ostream & os, const DoubleVector & v)
 
 int main()
 {
-    cout << endl;
     auto a = dye::on_yellow(42);
-    cout << "  " <<  a << endl;
+    cout << a << endl;
 
     using vec = DoubleVector;
 
     auto b = dye::red(vec{1, 2, 3});
     b = b + dye::blue(vec{4, 5, 6});
     b += dye::green(vec{7, 8, 9});
-    cout << "  " <<  b << endl;
+    cout <<  b << endl;
 
-    cout << "  " <<  dye::on_white(string("strings")) + " are " +
+    cout << dye::on_white(string("strings")) + " are " +
             dye::on_white("more") + string(" flexible") << endl;
 
-    cout << "  " <<  dye::colorize("grape", "purple") << endl;
+    cout << dye::colorize("grape", "purple") << endl;
 
-    cout << "  " <<  dye::inverse(dye::red("red")) << endl;
+    cout << dye::inverse(dye::red("red")) << endl;
 
     auto contrast = dye::vanilla("contrast");
-    cout << "  " <<  contrast.inverse() << endl;
+    cout << contrast.inverse() << endl;
 
     return 0;
 }
